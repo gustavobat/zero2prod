aa75c79b585480bc455a635b53c6c06884e21435
Gustavo Batistela <gustavoabatistela@gmail.com>
2022-04-11 12:24:47 -0300

    Spawn app at random available port

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 //! main.rs
 
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    run(listener)?.await
 }
