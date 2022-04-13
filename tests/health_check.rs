5e0e2ed399cdf70f38c1afa7f02a7e529e501e67
Gustavo Batistela <gustavoabatistela@gmail.com>
2022-04-13 16:22:21 -0300

    Query DB in test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,5 @@
 //! tests/health_check.rs
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -42,6 +43,13 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request.");
 
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[actix_rt::test]
