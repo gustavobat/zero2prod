118770ff718ac868f0dc0bfc83ed8225397056a9
Gustavo Batistela <gustavoabatistela@gmail.com>
2022-04-13 12:21:19 -0300

    Add FormData serialization

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,12 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
